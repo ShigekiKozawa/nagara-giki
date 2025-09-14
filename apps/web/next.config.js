548cfa7e8b0c5a5313a18225a8877804df59ba11
ShigekiKozawa <sigekikozawa@aol.com>
2025-09-14 18:51:41 +0900

    fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,12 +6,28 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nagara-giki' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nagara-giki/' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/nagara-giki' : '',
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
     NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000',
   },
+  async generateBuildId() {
+    return 'build'
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig 
